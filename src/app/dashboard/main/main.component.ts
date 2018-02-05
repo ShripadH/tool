008baa20b1bde64550309f2b833d59bd49f01e78
ShripadH <hendre.shripad@yahoo.com>
2018-02-05 22:39:32 +0530

    resolving errors

--- src/app/dashboard/main/main.component.ts
+++ src/app/dashboard/main/main.component.ts
@@ -22,7 +22,7 @@ export class MainComponent implements OnInit {
 
   controls: Control[];
   depControls: Control[];
-
+  style1 = 'col-sm-6 col-md-6 col-lg-6';
   IsVisible: boolean = true;
   controlMapper: ControlComponentMap[];
 
@@ -33,8 +33,14 @@ export class MainComponent implements OnInit {
     ];
 
     this.controls = [
-      { "Id": "wd1", "Label": "Widget 1", "Component": "SampleWidget1Component", "Options": null, "Value": null, "Dep": null },
-      { "Id": "wd2", "Label": "Widget 2", "Component": "SampleWidget2Component", "Options": null, "Value": null, "Dep": null }
+      { "Id": "wd1", "Label": "Widget 1", "Component": SampleWidget1Component, "Options": null, "Value": null, "Dep": null },
+      { "Id": "wd2", "Label": "Widget 2", "Component": SampleWidget2Component, "Options": null, "Value": null, "Dep": null },
+      { "Id": "wd1", "Label": "Widget 1", "Component": SampleWidget1Component, "Options": null, "Value": null, "Dep": null },
+      { "Id": "wd2", "Label": "Widget 2", "Component": SampleWidget2Component, "Options": null, "Value": null, "Dep": null },
+      { "Id": "wd1", "Label": "Widget 1", "Component": SampleWidget1Component, "Options": null, "Value": null, "Dep": null },
+      { "Id": "wd2", "Label": "Widget 2", "Component": SampleWidget2Component, "Options": null, "Value": null, "Dep": null },
+      { "Id": "wd1", "Label": "Widget 1", "Component": SampleWidget1Component, "Options": null, "Value": null, "Dep": null },
+      { "Id": "wd2", "Label": "Widget 2", "Component": SampleWidget2Component, "Options": null, "Value": null, "Dep": null }
     ]
 
     // this.apiService.getSchema().subscribe(data=>{
